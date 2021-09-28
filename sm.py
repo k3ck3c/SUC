c90c82921240306d9daa2c9dfd87a8fd76748d4a
k3ck3c <k3ck3c@gmail.com>
2021-09-28 09:58:07 +0200

    Add files via upload
    modifs mineures
    ajout des plus petits V2 et V3

--- sm.py
+++ sm.py
@@ -8,6 +8,8 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 from random import randint
 from time import sleep
+
+    
 listeUserAgents = [ 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; fr-fr) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1',
                                                 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1',
                                                 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13\
@@ -19,54 +21,60 @@ listeUserAgents = [ 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; fr-fr) Ap
                                                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.8 (KHTML, like Gecko) Chrome/17.0.940.0 Safari/535.8' ]
 
 url = 'https://www.tesla.com/findus/list/superchargers/France'
-bientot = 0
-cp = 0
-suc = 0
-sucv2 = 0
-sucv3 = 0
-nbsuc = 0
-bigv2 = 0
-bigv3 = 0
+bientot, cp, suc, sucv2, sucv3, nbsuc, bigv2, bigv3 = 0, 0, 0, 0, 0, 0, 0, 0
 sitev2 = ''
 sitev3 = ''
+smallv2= 10
+smallv3 = 10
+petitv2 = ''
+petitv3 = ''
+#if __name__ = "__main__":
 response = requests.get(url, headers={'user-agent': random.choice(listeUserAgents)})
 print response.status_code
 soup = BeautifulSoup(response.content, 'html.parser')
-for x in soup.find_all('a'):
-    if '/findus/location/supercharger' in x['href']:
-        if '(coming soon)' in x.text:
+for lien in soup.find_all('a'):
+    if '/findus/location/supercharger' in lien['href']:
+        if '(coming soon)' in lien.text:
             bientot = bientot + 1
             print 'il y a ',bientot,'SUC annonces '
-            print x.text
+            print lien.text
             print '*' * 80
         else:
-            print x.text
-            print x['href']
+            print lien.text
+#            print lien['href']
             cp = cp + 1
             sleep(randint(3,12)) #pour éviter de se faire blacklister par les sites Tesla (erreur 403)
             print 'numero ',cp
-            urlsuc = 'https://www.tesla.com/fr_FR' + x['href']
-            print 'le site local ',urlsuc
+            urlsuc = 'https://www.tesla.com/fr_FR' + lien['href']
+#            print 'le site local ',urlsuc
             htmlsuc = requests.get(urlsuc, headers={'user-agent': random.choice(listeUserAgents)})
             print htmlsuc.ok
             if htmlsuc.ok:
                 soupsuc = BeautifulSoup(htmlsuc.content, 'html.parser')
-                for y in soupsuc.find(attrs={ "class":"vcard"}).findAll(text=True):
-                    if 'upercharger' in  y:
-                        print y.split(' ')[0], y.split(' ')[-1]
-                        print 'nombre de stalles ', y.split(' ')[0]
-                        if '150' in y:
-                            sucv2 = sucv2 + int(y.split(' ')[0])
-                            if  int(y.split(' ')[0]) > bigv2:
-                                bigv2 = int(y.split(' ')[0])
-                                sitev2 = x.text
-                            print y.split(' ')[0], ' SUC V2'
-                        if '250' in y:
-                            sucv3 = sucv3 + int(y.split(' ')[0])
-                            if int(y.split(' ')[0]) > bigv3:
-                                bigv3 = int(y.split(' ')[0])
-                                sitev3 = x.text
-                            print y.split(' ')[0], ' SUC V3'
+                for infosuc in soupsuc.find(attrs={ "class":"vcard"}).findAll(text=True):
+                    if 'upercharger' in  infosuc:
+                        stalle = infosuc.split(' ')[0]
+                        print stalle, infosuc.split(' ')[-1]
+                        print 'nombre de stalles ', stalle
+                        if '150' in infosuc:
+                            sucv2 = sucv2 + int(stalle)
+                            print sucv2
+                            if  int(stalle) > bigv2:
+                                bigv2 = int(stalle)
+                                sitev2 = lien.text
+                            if int(stalle) < smallv2:
+                                smallv2 = int(stalle)
+                                petitv2 = lien.text
+                            print stalle, ' SUC V2'
+                        if '250' in infosuc:
+                            sucv3 = sucv3 + int(stalle)
+                            if int(stalle) > bigv3:
+                                bigv3 = int(stalle)
+                                sitev3 = lien.text
+                            if int(stalle) < smallv3:
+                                smallv3 = int(stalle)
+                                petitv3 = lien.text
+                            print stalle, ' SUC V3'
 
             print '%s: %d' % ('nombre de stalles V2 ' , sucv2)
             print '%s: %d' % ('nombre de stalles V3 ' , sucv3)
@@ -81,3 +89,6 @@ print 'il y a ', sucv3, ' stalles à 250 kW'
 print 'il y a un total de ', sucv2 + sucv3 , 'stalles'
 print 'le plus grand V2 a',  bigv2, 'stalles à ', sitev2
 print 'le plus grand V3 a',  bigv3, 'stalles à ', sitev3
+print 'le plus petit V2 a', smallv2, 'stalles à ', petitv2
+print 'le plus petit V3 a', smallv3, 'stalles à ', petitv3
+#    pass
